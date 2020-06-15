35ec873cb0cb65ed73a94b05f88a123ceeca6ff1
xiaozhi <zhijiezy@163.com>
2020-06-15 11:43:02 +0800

    第一次提交 springboot

--- src/main/java/zzj/cn/springboottest/SpringbootTestApplication.java
+++ src/main/java/zzj/cn/springboottest/SpringbootTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootTestApplication {
 
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(SpringbootTestApplication.class, args);
     }
 
